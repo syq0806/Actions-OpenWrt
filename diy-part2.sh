80136d24d26da80dad7adf44f72eb7286b38b7af
syq0806 <61413903+syq0806@users.noreply.github.com>
2022-08-31 22:26:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,25 +9,9 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
 
-#openclash packages
-git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
-#passwall package 
-git clone  https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
-git clone -b luci  https://github.com/xiaorouji/openwrt-passwall.git  package/luci-app-passwall
-
-#helloworld package
-git clone -b master  https://github.com/fw876/helloworld.git  package/helloworld
-#for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt"  "redsocks2"; do \
-#  svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
-#done
-svn checkout https://github.com/immortalwrt/packages/trunk/net/redsocks2    package/helloworld/redsocks2
-
-#CDN speedtest package
-git clone https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest.git package/luci-app-cloudflarespeedtest
-git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git package/cdnspeedtest
 
 #update golang
 pushd feeds/packages/lang
